e72ba887dfd08eae67a2e18e4b3cb5c54ccba59b
Joeri Sykora <joeri@sertik.net>
2023-02-24 16:55:38 +0100

    use openssl on macos and ios

--- src/lib.rs
+++ src/lib.rs
@@ -98,26 +98,19 @@
 #![warn(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
-#[macro_use]
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-extern crate lazy_static;
-
 use std::any::Any;
 use std::error;
 use std::fmt;
 use std::io;
 use std::result;
 
-#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
+#[cfg(not(target_os = "windows"))]
 #[macro_use]
 extern crate log;
-#[cfg(any(target_os = "macos", target_os = "ios"))]
-#[path = "imp/security_framework.rs"]
-mod imp;
 #[cfg(target_os = "windows")]
 #[path = "imp/schannel.rs"]
 mod imp;
-#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "ios")))]
+#[cfg(not(target_os = "windows"))]
 #[path = "imp/openssl.rs"]
 mod imp;
 
